992ba784d7ed406e17a85e17ca647c7f2ed0a34d
Greg Bell <greg@bitwombat.com.au>
2015-08-11 04:39:26 +1000

    Create test data and modify tests to run locally.
    - Create test suite with 1000s of files - don't worry, they're 17 bytes each.
    - Change references to ../../.. and /Users/james which prevented the tests from
    working anywhere other than the original dev's machine.
    - Remove empty FilesystemTest.php, which prevented tests from passing.
    - Enhance tests to check number of files actually returned.

--- tests/deit/filesystem/FinderTest.php
+++ tests/deit/filesystem/FinderTest.php
@@ -6,11 +6,16 @@ namespace deit\filesystem;
  * Finder test
  * @author James Newell <james@digitaledgeit.com.au>
  */
+
+$total_files    = 5654;
+$total_js_files = 682;
+$total_folders  = 879;
+
 class FinderTest extends \PHPUnit_Framework_TestCase {
 
 	public function test_files() {
 
-		$finder = new Finder('../../../');
+		$finder = new Finder('tests/test-data/bigtree');
 		foreach ($finder->files() as $path) {
 			if (!$path->isFile()) {
 				$this->fail("Path {$path->getPathname()} is not a file");
@@ -21,8 +26,13 @@ class FinderTest extends \PHPUnit_Framework_TestCase {
 
 	public function test_files_named() {
 
-		$finder = new Finder('../../../');
-		foreach ($finder->files()->named('#\.json#') as $path) {
+		global $total_files;
+
+		$finder = new Finder('tests/test-data/bigtree');
+		
+		$this->assertEquals($total_files, count($finder->files()));
+
+		foreach ($finder->files()->named('#\.json$#') as $path) {
 			if (!$path->isFile() || $path->getExtension() != 'json') {
 				$this->fail("Path {$path->getPathname()} is not a JSON file");
 			}
@@ -32,7 +42,12 @@ class FinderTest extends \PHPUnit_Framework_TestCase {
 
 	public function test_folders() {
 
-		$finder = new Finder('../../../');
+		global $total_folders;
+
+		$finder = new Finder('tests/test-data/bigtree');
+
+		$this->assertEquals($total_folders, count($finder->folders()));
+
 		foreach ($finder->folders() as $path) {
 			if (!$path->isDir()) {
 				$this->fail("Path {$path->getPathname()} is not a folder");
@@ -45,11 +60,11 @@ class FinderTest extends \PHPUnit_Framework_TestCase {
 
 		$fs = new Filesystem();
 
-		$f = new Finder('../../../');
+		$f = new Finder('tests/test-data/bigtree');
 		$f
 			->files()
 			->named('#\.json#')
-			->copyTo('/Users/james/tmp/test')
+			->copyTo('tests/test-data-copy')
 		;
 
 	}
